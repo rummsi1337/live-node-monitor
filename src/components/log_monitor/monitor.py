fc05c581a8d3d056eecc477beca9da99166f4bb9
Simon Rummert <09.milieu.motes@icloud.com>
2024-01-20 20:03:31 +0000

    Store log monitor data in elasticsearch

--- src/components/log_monitor/monitor.py
+++ src/components/log_monitor/monitor.py
@@ -2,36 +2,56 @@ from typing import Iterator, Optional
 import asyncio
 from aiofile import async_open
 import glob
+
+from elasticsearch import AsyncElasticsearch
 from components.log_monitor.utils import read_config
-from components.log_monitor.model import LogMonitorEvent
-from components.log_monitor.logparser import LogEventParser
+from components.log_monitor.model import LogMonitorEvent, Target, TargetType
 
 
 class LogMonitor:
-    def __init__(self, log_file_path: str, log_events: list[LogMonitorEvent]) -> None:
+    def __init__(
+        self,
+        log_file_path: str,
+        log_events: list[LogMonitorEvent],
+        es: AsyncElasticsearch,
+    ) -> None:
         self.log_file_path = log_file_path
-        self.event_parsers = []
-        for event in log_events:
-            self.event_parsers.append(LogEventParser(event.name, event.regexes))
+        self.events = log_events
+        self.es = es
 
     async def start_monitoring(self):
         async with async_open(self.log_file_path, "r") as logfile:
             loglines = self._follow(logfile)
             async for line in loglines:
-                parsed_line = self._retrieve_line_event(line)
-                if parsed_line:
-                    # TODO: Send each parsed line event to the api service
-                    print(parsed_line)
+                try:
+                    parsed_line = await self._retrieve_line_event(line)
+                    if parsed_line:
+                        print(parsed_line)
+                except Exception as e:
+                    # TODO: proper exception handling. Make sure the parser can continue but properly logs the errors.
+                    print(e)
 
-    def _retrieve_line_event(self, line: str) -> Optional[dict]:
+    async def _retrieve_line_event(self, line: str) -> Optional[dict]:
         """
         Find line events and parse corresponding log line.
         Return dict of line event if found, otherwise None
         """
-        for parser in self.event_parsers:
-            if result := parser.parse_line(line):
+        for event in self.events:
+            if result := event.parser.parse_line(line):
+                await self._save_data(result, event.targets)
                 return result
         return None
+
+    async def _save_data(self, data: dict, targets: list[Target]):
+        for target in targets:
+            if target.name == TargetType.ELASTICSEARCH:
+                response = await self._save_data_es(data, target.config)
+                print(response)
+
+    async def _save_data_es(self, data: dict, config: dict):
+        # TODO: sending documents individually or sending them in bulk?
+        # Can the mechanism be rebuilt to use https://elasticsearch-py.readthedocs.io/en/v8.12.0/async.html#bulk-and-streaming-bulk
+        return await self.es.index(index=config["index"], document=data)
 
     @staticmethod
     async def _follow(file, sleep_timeout=0.1) -> Iterator[str]:
@@ -55,10 +75,10 @@ async def start_monitors(log_monitors: list[LogMonitor]):
     await asyncio.gather(*tasks)
 
 
-def run_log_monitor(config_path: str):
+def run_log_monitor(config_path: str, es: AsyncElasticsearch):
     configs = read_config(config_path)
     log_monitors = []
     for config in configs.log_configs:
         for path in glob.glob(config.path):
-            log_monitors.append(LogMonitor(path, config.events))
+            log_monitors.append(LogMonitor(path, config.events, es))
     asyncio.run(start_monitors(log_monitors))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import os
+from elasticsearch import AsyncElasticsearch
 from components.log_monitor.monitor import run_log_monitor
 
 
 if __name__ == "__main__":
-    run_log_monitor("/workspaces/live-node-monitor/log_monitor_config.yaml")
+    # TODO: use proper logging framework
+    # TODO: get config vars from argparse, env or file
+    config_path = "/workspaces/live-node-monitor/log_monitor_config.yaml"
+    es_host = os.getenv("ES_HOST")
+    # TODO: How to do basic auth using netrc? does this work out-of-the-box?
+    es_username = os.getenv("ES_USERNAME")
+    es_password = os.getenv("ES_PASSWORD")
+    es = AsyncElasticsearch(
+        es_host, basic_auth=(es_username, es_password), verify_certs=False
+    )
+    run_log_monitor(config_path, es)
